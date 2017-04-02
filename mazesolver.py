4ffba85ca3366eaf8c848a99698cd5b0c30c4c1e
djin31 <deepanshujindal.99@gmail.com>
2017-04-02 14:15:30 +0530

    Update mazesolver.py

--- mazesolver.py
+++ mazesolver.py
@@ -1,19 +1,23 @@
 import time,pdb
 p=[]
-a=[	[1,1,1,0,0],
-	[0,0,1,0,0],
-	[1,1,1,1,0],
-	[1,0,0,0,1],
-	[1,1,1,1,1]] #matrix
+a=[[1,1,1,1,1,1],
+   [1,0,1,0,0,1],
+   [1,0,1,0,0,1],
+   [1,1,1,0,0,1],
+   [0,0,0,0,0,1],
+   [1,0,1,1,1,1]] #matrix
 n=len(a)
 prev=(0,0)
 curr=(0,0)
 b=a
 def backtracker():
-	global p
-        while 1:
-		curr=p[-2]
-		p=p[:-2]
+  global p
+  #print p
+  while 1:
+		s=len(p)
+		curr=p[s-2]
+		#print curr
+		p=p[:-1]
 		flag=1	
 		if curr[0]<n-1:	
 			if b[curr[0]+1][curr[1]]==1:
@@ -35,6 +39,11 @@ def backtracker():
 				prev=curr
 				curr=(curr[0],curr[1]-1)
 				return curr,prev
+		#print p
+		if len(p)==1:
+		  print "Are you kidding me!"
+		  break
+		  
 
 
 
@@ -46,7 +55,7 @@ while 1:
 	print curr
 	flag=1	
 	if curr==(n-1,n-1):
-			print "Woah!"
+			print "Woah!\n"
 			break
 
 
@@ -72,9 +81,23 @@ while 1:
 			flag=0
 	if flag==1:
 		if curr==(n-1,n-1):
-			print "Woah!"
+			print "Woah!\n"
 			break
 		else:
-			curr,prev=backtracker()
-	time.sleep(1)
+		  z=backtracker()
+		  if z==None :
+			  print "No way home!"
+			  break
+		  curr,prev=z[0],z[1]
+			
+	time.sleep(0.5)
+	
+for i in range(n):
+  for j in range(n):
+    if(i,j) in p:
+      print 1,
+    else:
+      print " ",
+    
+  print " "
 		
