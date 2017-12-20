e9ca4fe024228c9e4bce6c912ceaeac976d0e585
djin31 <deepanshujindal.99@gmail.com>
2017-12-20 13:22:11 +0530

    Improved version
    Time complexity of O(n^2) to find all the prime numbers in range 1 to n

--- eratosthenes.py
+++ eratosthenes.py
@@ -1,18 +1,7 @@
 #eratosthenes sieve
-def sieve(n):
-	l=[False,False]
-	for i in xrange(2,n+1):
-		l.append(True) 		#init
-	for i in xrange(2,n+1):
-		if (l[i]):
-			for j in xrange(i+1,n+1):
-				if j%i==0:
-					l[j]=False
-	s=[i for i in range(n+1) if l[i]==True]
-	print s
-
-
-#improvement over it
+#improved version
+#print prime numbers uptil a certain number 
+#pass the input to the given function the number upto which you want to print prime numbers
 
 def sieve(n):
 	l=[False,False]
